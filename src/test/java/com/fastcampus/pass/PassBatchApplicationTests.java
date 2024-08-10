b2a16c7f4ad7a6cb691591308406279cbc579b8a
hingjae <ljh23270@naver.com>
2024-08-10 20:46:34 +0900

    - docker compose 파일 환경변수 분리 - 테스트 용 환경 구성 - BatchConfig - JPAConfig

--- src/test/java/com/fastcampus/pass/PassBatchApplicationTests.java
+++ src/test/java/com/fastcampus/pass/PassBatchApplicationTests.java
@@ -2,7 +2,9 @@ package com.fastcampus.pass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PassBatchApplicationTests {
 
